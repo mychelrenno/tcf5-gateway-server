bc340432432303c73b02908b50036aadd5fe0ab8
Mychel Renno <mychelrenno@gmail.com>
2026-03-28 11:32:23 -0300

    criado projeto inicial

---
+++ src/main/java/com/devrenno/tcf5/gateway/Tcf5GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.devrenno.tcf5.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tcf5GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tcf5GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devrenno/tcf5/gateway/Tcf5GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrenno.tcf5.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tcf5GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
